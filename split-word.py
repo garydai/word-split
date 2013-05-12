e10a3b2e117a7648952b9a2503e16d283a3115b8
garydai <techang2009@126.com>
2013-05-12 23:18:02 +0800

    update hmm algorithm

--- split-word.py
+++ split-word.py
@@ -3,33 +3,33 @@ import sys
 #print sys.getdefaultencoding()
 import codecs
 #print u'Hello world! 你好，世界！'
+import math
 
 import collections
 dic=collections.defaultdict(lambda:1)
-#隐藏状态
-states = {'s':0, 'e':1, 'm':2, 'a':3}
+
 transition_probility = [[0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]]
-initial_probility = {'b':0.0, 'm':0.0, 'e':0.0, 's':0.0}
+initial_states = {'b':0.0, 'm':0.0, 'e':0.0, 's':0.0}
 emit_probility = {'b':{}, 'm':{}, 'e':{}, 's':{}}
 transition_probility = {
-'b':{'b':0.0}, 'b':{'m':0.19922840916814916 }, 'b':{'e':0.8007715908318509}, 'b':{'s':0.0},
-'m':{'b':0.0}, 'b':{'m':0.47583202978061256  }, 'b':{'e':0.5241679702193874 }, 'b':{'s':0.0},
-'e':{'b':0.6309567616935934}, 'b':{'m':0.0 }, 'b':{'e':0.0}, 'b':{'s':0.36904323830640656},
-'s':{'b':0.6343402140354506}, 'b':{'m':0.0 }, 'b':{'e':0.0}, 'b':{'s':0.36565844303914763},
+'b':{'b':0.0, 'm':0.19922840916814916 , 'e':0.8007715908318509, 's':0.0},
+'m':{'b':0.0, 'm':0.47583202978061256 , 'e':0.5241679702193874 , 's':0.0},
+'e':{'b':0.6309567616935934, 'm':0.0 , 'e':0.0, 's':0.36904323830640656},
+'s':{'b':0.6343402140354506, 'm':0.0 , 'e':0.0, 's':0.36565844303914763},
 }
 #def create_emit_matrix(word):
 
 #该函数错的,不能只有一个词语决定转移函数,要根据句子的分词情况,e->s也是有概率的
 def create_transiton_matrix(word):
 	global transition_probility_count
-	global initial_probility
+	global initial_states
 	global transition_probility
 	#单字
 	if len(word) == 1:
-		initial_probility['s'] = initial_probility['s'] + 1
+		initial_states['s'] = initial_states['s'] + 1
 	#多字	
 	if len(word) > 1:
-		initial_probility['b'] = initial_probility['b'] + 1
+		initial_states['b'] = initial_states['b'] + 1
 
 
 def LoadDic(filename):
@@ -47,26 +47,27 @@ def LoadDic(filename):
 				trie[word]['s'] = 	trie[word]['s'] + 1
 				trie[word]['sum'] = trie[word]['sum'] + 1
 			else:
-				trie[word] = {'s':1.0, 'b':0.0, 'e':0.0, 'm':0.0, 'sum':1.0, 'char':word}
+				trie[word] = {'s':float(frq), 'b':0.0, 'e':0.0, 'm':0.0, 'sum':float(frq), 'char':word}
 			continue
 		i = 0
 		for c in word:
 			if trie.has_key(c):
 				if i == 0:
-					trie[c]['b'] = trie[c]['b'] + 1
+					trie[c]['b'] = trie[c]['b'] + float(frq)
 				elif i < len(word) - 2:
-					trie[c]['m'] = trie[c]['m'] + 1
+					trie[c]['m'] = trie[c]['m'] + float(frq)
 				elif i == len(word) - 1:
-					trie[c]['e'] = trie[c]['e'] + 1
-				trie[c]['sum'] = trie[c]['sum'] + 1
+					trie[c]['e'] = trie[c]['e'] + float(frq)
+				trie[c]['sum'] = trie[c]['sum'] + float(frq)
 			else:
 				if i == 0:
-					trie[c] = {'s':0.0, 'b':1.0, 'e':0.0, 'm':0.0, 'sum':1.0, 'char':c}
+					trie[c] = {'s':0.0, 'b':float(frq), 'e':0.0, 'm':0.0, 'sum':float(frq), 'char':c}
 				elif i < len(word) - 2:
-					trie[c] = {'s':0.0, 'b':0.0, 'e':0.0, 'm':1.0, 'sum':1.0, 'char':c}
+					trie[c] = {'s':0.0, 'b':0.0, 'e':0.0, 'm':float(frq), 'sum':float(frq), 'char':c}
 				elif i == len(word) - 1:
-					trie[c] = {'s':0.0, 'b':0.0, 'e':1.0, 'm':0.0, 'sum':1.0, 'char':c}
+					trie[c] = {'s':0.0, 'b':0.0, 'e':float(frq), 'm':0.0, 'sum':float(frq), 'char':c}
 			i = i + 1
+	#print trie[u'啊']
 	global emit_probility
 	for c in trie:
 		emit_probility['s'][c] = trie[c]['s'] / trie[c]['sum']
@@ -81,12 +82,81 @@ def LoadDic(filename):
 			string = u'%s%f\n'%(j,emit_probility[i][j])
 			f.write(string.encode('utf-8'))
 	
-	summ = initial_probility['s'] + initial_probility['b'] 
-	initial_probility['b'] = initial_probility['b'] / summ
-	initial_probility['s'] = initial_probility['s'] / summ	
-	print initial_probility['b'] ,initial_probility['s'] 
+	summ = initial_states['s'] + initial_states['b'] 
+	initial_states['b'] = initial_states['b'] / summ
+	initial_states['s'] = initial_states['s'] / summ	
+	#print initial_states['b'] ,initial_states['s'] 
 	#print trie		
 	dic[u'_t_'] = count
+
+
+def Viterbi(initial_states, emit_probility, transition_probility, sentence):
+	#print transition_probility
+	p = []
+	trace = []
+	for char in sentence:
+		p.append({'s':0.0, 'b':0.0, 'e':0.0, 'm':0.0})
+		trace.append({'s':'0', 'b':'0', 'e':'0', 'm':'0'})
+	states = 'besm'
+	for i in range(len(sentence)):
+		if i == 0:
+			p[0]['s'] = math.log(emit_probility['s'][sentence[0]]) + math.log(initial_states['s'])
+			p[0]['b'] = math.log(emit_probility['b'][sentence[0]]) + math.log(initial_states['b'])
+		else:
+			for now_states in states:
+				for last_stete in states:
+					if transition_probility[last_stete][now_states] == 0.0 or emit_probility[now_states][sentence[i]] == 0:
+						continue
+					#上一个状态没到达
+					if p[i - 1][last_stete] == 0:
+						continue
+
+					if p[i][now_states] == 0.0:
+						p[i][now_states] = p[i - 1][last_stete] \
+											+ math.log(emit_probility[now_states][sentence[i]]) \
+											+ math.log(transition_probility[last_stete][now_states])
+						trace[i][now_states] = last_stete
+					else:
+						if p[i - 1][last_stete] + math.log(emit_probility[now_states][sentence[i]]) \
+							+ math.log(transition_probility[last_stete][now_states]) > p[i][now_states]:
+							trace[i][now_states] = last_stete
+							p[i][now_states] = p[i - 1][last_stete] + math.log(emit_probility[now_states][sentence[i]]) \
+							+ math.log(transition_probility[last_stete][now_states])
+	#print p
+	#print emit_probility['s'][u'啊']
+	#print emit_probility['b'][u'啊']
+	char  = 's'
+	prot = p[len(sentence) - 1]['s']
+
+	#print p
+	for state in states:
+		if prot < p[len(sentence) - 1][state]:
+			prot = p[len(sentence) - 1][state]
+			char = state
+	output_state = ''
+	for i in range(len(sentence)- 1, -1, -1):
+	#	if char == 'e': 
+		#	print '|'
+		#if char == 's': 
+		#	print '|'
+		#if char == 'b':
+		#	print '|'
+		#print sentence[i]
+		#print char, trace[i][char]		
+		#print char
+		output_state = char + output_state 
+		char = trace[i][char]
+	j = 0
+	s = ''
+	for i in output_state:
+		if i == 'b' or i == 's' :
+			s =  s +  '|' + sentence[j]
+		elif i == 's' or i == 'e':
+			s = s + sentence[j] + '|'
+		else:
+			s =  s + sentence[j] 
+		j = j + 1
+	print s
 
 
 def SplitWord(ipt):
@@ -144,7 +214,9 @@ if __name__ == '__main__':
 	LoadDic('dict.txt')
 	for line in file('word-test.txt'):
 		line = line.replace('\n', '')
+		#line  = '你好啊朋友'
 		SplitWord(line.decode('utf-8'))
+		Viterbi(initial_states, emit_probility, transition_probility, line.decode('utf-8'))
 
 
 
